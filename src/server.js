172879c609d282d05c0dac60d0a6dbfdcbd81d6a
Cyril Wanner <info@cyr.li>
2018-08-21 19:48:55 +0200

    Catch directory for the start command correctly

--- src/server.js
+++ src/server.js
@@ -15,7 +15,7 @@ const argv = parseArgs(process.argv.slice(2), {
 });
 
 const dev = process.env.NODE_ENV !== 'production';
-const dir = argv._[(argv._[0] === 'dev' ? 1 : 0)] || '.';
+const dir = argv._[(argv._[0] === 'dev' || argv._[0] === 'start' ? 1 : 0)] || '.';
 const app = next({ dev, dir });
 const handler = app.getRequestHandler();
 
